7bd29f955fc5764b96d0c22b76f83d9d35b8c17c
winstxnhdw <winstxnhdw@gmail.com>
2022-12-27 04:14:23 +0800

    fix: update SharpMonoInjector to working version

--- launch.bat
+++ launch.bat
@@ -1,9 +1,5 @@
 @echo off
 
-rmdir /s /q bin
-rmdir /s /q "raft-hax/obj"
-cls
-
 dotnet build raft-hax
 start /wait /b ./submodules/SharpMonoInjectorCore/bin/SharpMonoInjector.exe inject -p Raft -a bin/raft-hax.dll -n Hax -c Loader -m Load
 
